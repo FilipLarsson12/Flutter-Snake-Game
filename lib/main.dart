cc64f2b715431cc3a0940a6c7581aacb434ce822
Filip Larsson <hockeyfilip12@gmail.com>
2023-03-16 16:18:23 +0100

    adding better logic

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            xMovableContainerStateGlobalKey.currentState?.incrementDirection();
-            xMovableContainerStateGlobalKey.currentState?.startAnimation();
-          },
-          child: Stack(
+      home: GestureDetector(
+        onTap: () {
+          snakeHeadStateGlobalKey.currentState?.incrementDirection();
+          snakePartStateGlobalKey.currentState?.incrementDirection();
+          snakeHeadStateGlobalKey.currentState?.startAnimation();
+          snakePartStateGlobalKey.currentState?.startAnimation();
+        },
+        child: Scaffold(
+          body: Stack(
             children: [
-              xMovableContainer(
+              snakeHead(
                 direction: 0,
                 left: 100,
                 top: 100,
-                key: xMovableContainerStateGlobalKey,
+                key: snakeHeadStateGlobalKey,
+              ),
+              snakePart(
+                lag: 0,
+                direction: 0,
+                left: 100 - 1,
+                top: 100,
+                key: snakePartStateGlobalKey,
               ),
             ],
           ),
@@ -33,11 +42,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class xMovableContainer extends StatefulWidget {
+class snakeHead extends StatefulWidget {
   final int direction;
   final double left;
   final double top;
-  xMovableContainer(
+  snakeHead(
       {required this.direction,
       required this.left,
       required this.top,
@@ -45,14 +54,14 @@ class xMovableContainer extends StatefulWidget {
       : super(key: key);
 
   @override
-  xMovableContainerState createState() => xMovableContainerState();
+  snakeHeadState createState() => snakeHeadState();
 }
 
-// Key för att komma åt xMovableContainerState från parent widget.
-final GlobalKey<xMovableContainerState> xMovableContainerStateGlobalKey =
-    GlobalKey<xMovableContainerState>();
+// Key för att komma åt snakeHeadState från parent widget.
+final GlobalKey<snakeHeadState> snakeHeadStateGlobalKey =
+    GlobalKey<snakeHeadState>();
 
-class xMovableContainerState extends State<xMovableContainer>
+class snakeHeadState extends State<snakeHead>
     with SingleTickerProviderStateMixin {
   late int direction;
   late double left;
@@ -65,7 +74,6 @@ class xMovableContainerState extends State<xMovableContainer>
     direction = widget.direction;
     left = widget.left;
     top = widget.top;
-
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
@@ -107,7 +115,95 @@ class xMovableContainerState extends State<xMovableContainer>
       top: top,
       child: Container(
         decoration: BoxDecoration(color: Colors.black),
-        width: 50,
+        width: 10,
+        height: 10,
+      ),
+    );
+  }
+}
+
+class snakePart extends StatefulWidget {
+  final int lag;
+  final int direction;
+  final double left;
+  final double top;
+  snakePart(
+      {required this.lag,
+      required this.direction,
+      required this.left,
+      required this.top,
+      Key? key})
+      : super(key: key);
+
+  @override
+  snakePartState createState() => snakePartState();
+}
+
+// Key för att komma åt snakePartState från parent widget.
+final GlobalKey<snakePartState> snakePartStateGlobalKey =
+    GlobalKey<snakePartState>();
+
+class snakePartState extends State<snakePart>
+    with SingleTickerProviderStateMixin {
+  late int lag;
+  late int direction;
+  late double left;
+  late double top;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    lag = widget.lag;
+    direction = widget.direction;
+    left = widget.left;
+    top = widget.top;
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    controller.addListener(() {
+      setState(() {
+        lag++;
+        if (((direction % 4) == 1) || (direction % 4 == 2) && lag < 1) {
+          left += 1;
+        } else if (((direction % 4) == 2) || (direction % 4 == 3) && lag < 1) {
+          top += 1;
+        } else if (((direction % 4) == 3) || (direction % 4 == 0) && lag < 1) {
+          left -= 1;
+        } else if (((direction % 4) == 0) || (direction % 4 == 1) && lag < 1) {
+          top -= 1;
+        }
+      });
+    });
+  }
+
+  void incrementDirection() {
+    direction++;
+    lag = 0;
+  }
+
+  void startAnimation() {
+    setState(() {
+      controller.repeat();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: left,
+      top: top,
+      child: Container(
+        decoration: BoxDecoration(color: Colors.black),
+        width: 10,
         height: 10,
       ),
     );
